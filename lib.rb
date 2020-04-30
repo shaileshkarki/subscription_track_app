fbeaa3a1f1395fbe6807086ca2f201c44fffa819
Shailesh <shlshkrk@gmail.com>
2020-05-01 00:16:10 +1000

    created function for calculating monthly fee

--- lib.rb
+++ lib.rb
@@ -42,87 +42,166 @@ end
 
 
 def number_of_days_calculator(start_date, end_date)
-    start_date = Date.parse(start_date)
-    end_date = Date.parse(end_date)
+    start_date = Date.strptime(start_date)
+    end_date = Date.strptime(end_date)
     number_of_days = (end_date - start_date).to_i
     return number_of_days
 end
 
 def find_last_day_of_month(date)
+    if(date.class != Date)
+        date = Date.parse(date)
+    end
     last_day_of_month = Date.civil(date.year,date.month,-1)
-    return last_day_of_month.strftime("%Y-%m-01")
+    return last_day_of_month.strftime("%Y-%m-%d") 
 end
 #assuming billing cycle is based on 1 month = 30 days
-def calculate_expenses_for_given_month(id,date)
-    fee = 0.0
-    first_day_of_month = (date).strftime("%Y-%m-01")
-    # Date.strptime(date, "%Y-%m-01")
-    # Date.parse(date).strftime("%Y-%m-01")
-    last_day_of_month = find_last_day_of_month(date)
+# def calculate_expenses_for_given_month(id,date)
+#     fee = 0.0
+#     first_day_of_month = (date).strftime("%Y-%m-01")
+#     # Date.strptime(date, "%Y-%m-01")
+#     # Date.strptime(date).strftime("%Y-%m-01")
+#     last_day_of_month = find_last_day_of_month(date)
     
-    billing_start = 0
-    billing_end = 0
-    days_to_pay = 0
-    weeks_to_pay = 0
-    days_to_pay = 0
-    # days_in_given_month = number_of_days_calculator(first_day_of_month,last_day_of_month)
+#     billing_start = 0
+#     billing_end = 0
+#     days_to_pay = 0
+#     weeks_to_pay = 0
+#     days_to_pay = 0
+#     # days_in_given_month = number_of_days_calculator(first_day_of_month,last_day_of_month)
+#     subscriptions = all_subscriptions(id)
+#     subscriptions.each do |subscription|
+#         if(subscription['recurring']==1)
+#             month_gap = ((Date.strptime(subscription['start_date'], '%Y/%m/%d')).month-Date.strptime(first_day_of_month, '%Y/%m/%d').month)
+#             if(month_gap == 0)
+#                 fee += subscription['price'].to_f
+#             end
+#         elsif(subscription['recurring']==2)
+#             month_gap = ((Date.strptime(subscription['start_date'], '%Y/%m/%d')).month-Date.strptime(first_day_of_month, '%Y/%m/%d').month)
+#             if(month_gap == 0 ||month_gap == 6 || month_gap == -6 )
+#                 fee += subscription['price'].to_f
+#             end
+#         elsif(subscription['recurring']==3)
+#             month_gap = ((Date.strptime(subscription['start_date'], '%Y/%m/%d')).month-Date.strptime(first_day_of_month, '%Y/%m/%d').month)
+#             if(month_gap == 0 ||month_gap == 3 || month_gap == -3 )
+#                 fee += subscription['price'].to_f
+#             end
+#         elsif(subscription['recurring']==4)
+#             month_gap = ((Date.strptime(subscription['start_date'], '%Y/%m/%d')).month-Date.strptime(first_day_of_month, '%Y/%m/%d').month)
+#             if(month_gap == 0 ||month_gap == 2 || month_gap == -2 )
+#                 fee += subscription['price'].to_f
+#             end
+#         elsif(subscription['recurring']==5)
+#             if(Date.strptime(subscription['start_date'], '%Y/%m/%d').between?(first_day_of_month, last_day_of_month) && number_of_days_calculator(Date.strptime(last_day_of_month, '%Y/%m/%d'),Date.strptime(subscription['start_date'], '%Y/%m/%d'))<15)
+#                 fee += subscription['price'].to_f
+#             else
+#                 fee += (subscription['price'].to_f*2)
+#             end
+#         elsif(subscription['recurring']==6)
+#             billing_end = 0
+#             if(Date.strptime(subscription['start_date'], '%Y/%m/%d').between?(Date.strptime(first_day_of_month, '%Y/%m/%d'),Date.strptime(last_day_of_month, '%Y/%m/%d')))
+#                 weeks_to_pay = ((number_of_days_calculator(Date.strptime(last_day_of_month, '%Y/%m/%d'),Date.strptime(subscription['start_date'], '%Y/%m/%d')))/7).ceil
+#             else
+#                 billing_start = number_of_days_calculator(Date.strptime(firstst_day_of_month, '%Y/%m/%d'),Date.strptime(subscription['start_date'], '%Y/%m/%d'))
+#                 billing_end = number_of_days_calculator(Date.strptime(last_day_of_month, '%Y/%m/%d'),Date.strptime(subscription['start_date'], '%Y/%m/%d'))
+#                 weeks_to_pay = ((billing_end - billing_start)/7).ceil
+#             end
+#             fee += (subscription['price'].to_f*weeks_to_pay)
+#         elsif(subscription['recurring']==7)
+#             fee += subscription['price'].to_f
+#         elsif(subscription['recurring']==8)
+#             if(Date.strptime(subscription['start_date'], '%Y/%m/%d').between?(Date.strptime(first_day_of_month, '%Y/%m/%d'), Date.strptime(last_day_of_month, '%Y/%m/%d')))
+#                 days_to_pay = number_of_days_calculator(Date.strptime(last_day_of_month, '%Y/%m/%d'),Date.strptime(subscription['start_date'], '%Y/%m/%d'))
+#             else
+#                 billing_start = number_of_days_calculator(Date.strptime(first_day_of_month, '%Y/%m/%d'),Date.strptime(subscription['start_date'], '%Y/%m/%d'))
+#                 billing_end = number_of_days_calculator(Date.strptime(last_day_of_month, '%Y/%m/%d'),Date.strptime(subscription['start_date'], '%Y/%m/%d'))
+#                 days_to_pay = billing_end - billing_start
+#             end
+#             fee += (subscription['price'].to_f*days_to_pay)
+#         end
+#     end
+#     return fee
+# end
+# Date.new(2020,4,01)-Date.new(2018,5,20)
+# Date.new(2018,5,20).next_day(683).to_s
+# Date.new(2018,5,20).next_day((Date.strptime("2020-05-19")).year-Date.new(2018,5,20).year)*365)
+# year_gap = ((Date.strptime("2020-04-19")).year-Date.new(2018,10,20).year)
+# month_gap = ((Date.strptime("2020-04-19")).month-Date.new(2018,10,20).month)
+# Date.new(2018,10,20).next_month((year_gap*12)+(month_gap+6)).to_s
+# Date.new(2018,10,20).next_day(result).between?(Date.today, Date.civil(2020,4,-1))
+
+#Date.strptime(find_last_day_of_month(Date.today), '%Y-%m-%d')
+# def string_to_date(string_date)
+#     # if(string_date.class != Date)
+#         string_date = Date.strptime(string_date, '%Y-%m-%d')
+#     # end
+#     # string_date
+# end
+def create_an_array_of_dates(start_date,start_month_date,end_month_date,num)
+    start_date = start_date.to_s
+    end_month_date = end_month_date.to_s
+    start_month_date =start_month_date. to_s
+    num = days_interval(num).to_i
+    date_array = []
+    if(start_date <= end_month_date)
+        while Date.parse(start_date) <= Date.parse(end_month_date)  do
+            next_time = Date.parse(start_date).next_day(num).to_s
+            if(Date.parse(next_time)>=Date.parse(start_month_date)&&Date.parse(next_time)<=Date.parse(end_month_date) )
+                date_array.push(next_time)
+            end
+            start_date  = next_time
+        end
+    end
+    date_array       
+end
+
+
+# create_an_array_of_dates("2019-04-1","2020-04-1","2020-04-30",7)
+
+def days_interval(num)
+    # num is recurring
+    # num = num.to_i
+    if (num == 1 || num == "1")
+        # result = "Year"
+        days = 365
+    elsif (num == 2 || num == "2")
+        # result = "Half Year"
+        days = 182
+    elsif (num == 3 || num == "3")
+        # result = "Quarter"
+        days = 91
+    elsif (num == 4 || num == "4")
+        # result = "Bi-month"
+        days = 360
+    elsif (num == 5 || num == "5")
+        # result = "Fort Night"
+        days = 14
+    elsif (num == 6 || num == "6")
+        # result = "Week"
+        days = 7
+    elsif (num == 7 || num == "7")
+        # result = "Month"
+        days = 30
+    elsif (num == 8 || num == "8")
+        days = 1
+        # result = "Day"
+    end
+    days
+end
+
+def total_fee_for_current_month(id)
+    fee = 0.0 
+    last_day_of_month = find_last_day_of_month((Date.today).to_s) #gives date in string format
+    first_day_of_month = Date.parse(last_day_of_month).strftime("%Y-%m-01") #gives date in string format
     subscriptions = all_subscriptions(id)
     subscriptions.each do |subscription|
-        if(subscription['recurring']==1)
-            month_gap = ((Date.parse(subscription['start_date'])).month-Date.parse(first_day_of_month).month)
-            if(month_gap == 0)
-                fee += subscription['price']
-            end
-        elsif(subscription['recurring']==2)
-            month_gap = ((Date.parse(subscription['start_date'])).month-Date.parse(first_day_of_month).month)
-            if(month_gap == 0 ||month_gap == 6 || month_gap == -6 )
-                fee += subscription['price']
-            end
-        elsif(subscription['recurring']==3)
-            month_gap = ((Date.parse(subscription['start_date'])).month-Date.parse(first_day_of_month).month)
-            if(month_gap == 0 ||month_gap == 3 || month_gap == -3 )
-                fee += subscription['price']
-            end
-        elsif(subscription['recurring']==4)
-            month_gap = ((Date.parse(subscription['start_date'])).month-Date.parse(first_day_of_month).month)
-            if(month_gap == 0 ||month_gap == 2 || month_gap == -2 )
-                fee += subscription['price']
-            end
-        elsif(subscription['recurring']==5)
-            if(Date.parse(subscription['start_date']).between?(first_day_of_month, last_day_of_month) && number_of_days_calculator(Date.parse(last_day_of_month),Date.parse(subscription['start_date']))<15)
-                fee += subscription['price']
-            else
-                fee += (subscription['price']*2)
-            end
-        elsif(subscription['recurring']==6)
-            billing_end = 0
-            if(Date.parse(subscription['start_date']).between?(Date.parse(first_day_of_month),Date.parse(last_day_of_month)))
-                weeks_to_pay = ((number_of_days_calculator(Date.parse(last_day_of_month),Date.parse(subscription['start_date'])))/7).ceil
-            else
-                billing_start = number_of_days_calculator(Date.parse(firstst_day_of_month),Date.parse(subscription['start_date']))
-                billing_end = number_of_days_calculator(Date.parse(last_day_of_month),Date.parse(subscription['start_date']))
-                weeks_to_pay = ((billing_end - billing_start)/7).ceil
-            end
-            fee += (subscription['price']*weeks_to_pay)
-        elsif(subscription['recurring']==7)
-            fee += subscription['price']
-        elsif(subscription['recurring']==8)
-            if(Date.parse(subscription['start_date']).between?(Date.parse(first_day_of_month), Date.parse(last_day_of_month)))
-                days_to_pay = number_of_days_calculator(Date.parse(last_day_of_month),Date.parse(subscription['start_date']))
-            else
-                billing_start = number_of_days_calculator(Date.parse(first_day_of_month),Date.parse(subscription['start_date']))
-                billing_end = number_of_days_calculator(Date.parse(last_day_of_month),Date.parse(subscription['start_date']))
-                days_to_pay = billing_end - billing_start
-            end
-            fee += (subscription['price']*days_to_pay)
-        end
+        price = subscription['price'].to_f
+        start_date = subscription['start_date']
+        num = subscription['recurring']
+        array_of_pay_dates = create_an_array_of_dates(start_date,first_day_of_month,last_day_of_month,num)
+        fee += (price*array_of_pay_dates.count)
     end
     return fee
 end
-# Date.new(2020,4,01)-Date.new(2018,5,20)
-# Date.new(2018,5,20).next_day(683).to_s
-# Date.new(2018,5,20).next_day((Date.parse("2020-05-19")).year-Date.new(2018,5,20).year)*365)
-# year_gap = ((Date.parse("2020-04-19")).year-Date.new(2018,10,20).year)
-# month_gap = ((Date.parse("2020-04-19")).month-Date.new(2018,10,20).month)
-# Date.new(2018,10,20).next_month((year_gap*12)+(month_gap+6)).to_s
-# Date.new(2018,10,20).next_day(result).between?(Date.today, Date.civil(2020,4,-1))
+
+total_fee_for_current_month(5)
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,62 +1,62 @@
-var signup_form = document.forms["signup"]
-var new_form = document.forms["new"]
-var login_form = document.forms["login"]
+// var signup_form = document.forms["signup"]
+// var new_form = document.forms["new"]
+// var login_form = document.forms["login"]
 var edit_form = document.forms["edit"]
 var emailIsValid = function (email) {
     return /\S+@\S+\.\S+/.test(email)
 }  
-var validateSignupForm = function () {
-    var email = signup["email"].value;
-    var password = signup["password"].value;
-    var re_password = signup["re_password"].value;
-    if(!emailIsValid(email)) {
-        alert("Email is invalid. Try again!");
-        return false
-    }
-    if (password != re_password) {
-        alert("Password and Re-password did not match. Try again!");
-        return false;
-    }
-  }
-var validateNewForm = function () {
-    var  start_date = Date.parse(new_form["start_date"])
-    var cancel_date = Date.parse(new_form["cancel_date"])
-    if (cancel_date < start_date) {
-        alert("Cancel date cannot be earlier than start date. Try again!");
-        return false 
-    }
-}
-  var validateLoginForm = function () {
-    var email = login_form["email"].value;
-    if(!emailIsValid(email)) {
-        alert("Email is invalid. Try again!");
-        return false
-    }
-}
-var validateEditForm = function () {
-    var  start_date = Date.parse(edit_form["start_date"])
-    var cancel_date = Date.parse(edit_form["cancel_date"])
-    if (cancel_date < start_date) {
-        alert("Cancel date cannot be earlier than start date. Try again!");
-        return false 
-    }
-}
-var validateSignupForm = function () {
-    var email = signup["email"].value;
-    var password = signup["password"].value;
-    var re_password = signup["re_password"].value;
-    if(!emailIsValid(email)) {
-        alert("Email is invalid. Try again!");
-        return false
-    }
-    if (password != re_password) {
-        alert("Password and Re-password did not match. Try again!");
-        return false;
-    }
-  }
+// var validateSignupForm = function () {
+//     var email = signup["email"].value;
+//     var password = signup["password"].value;
+//     var re_password = signup["re_password"].value;
+//     if(!emailIsValid(email)) {
+//         alert("Email is invalid. Try again!");
+//         return false
+//     }
+//     if (password != re_password) {
+//         alert("Password and Re-password did not match. Try again!");
+//         return false;
+//     }
+//   }
+// var validateNewForm = function () {
+//     var  start_date = Date.parse(new_form["start_date"])
+//     var cancel_date = Date.parse(new_form["cancel_date"])
+//     if (cancel_date < start_date) {
+//         alert("Cancel date cannot be earlier than start date. Try again!");
+//         return false 
+//     }
+// }
+//   var validateLoginForm = function () {
+//     var email = login_form["email"].value;
+//     if(!emailIsValid(email)) {
+//         alert("Email is invalid. Try again!");
+//         return false
+//     }
+// }
+// var validateEditForm = function () {
+//     var  start_date = Date.parse(edit_form["start_date"])
+//     var cancel_date = Date.parse(edit_form["cancel_date"])
+//     if (cancel_date < start_date) {
+//         alert("Cancel date cannot be earlier than start date. Try again!");
+//         return false 
+//     }
+// }
+// var validateSignupForm = function () {
+//     var email = signup["email"].value;
+//     var password = signup["password"].value;
+//     var re_password = signup["re_password"].value;
+//     if(!emailIsValid(email)) {
+//         alert("Email is invalid. Try again!");
+//         return false
+//     }
+//     if (password != re_password) {
+//         alert("Password and Re-password did not match. Try again!");
+//         return false;
+//     }
+//   }
 
-signup_form.addEventListener('submit',validateSignupForm);
-new_form.addEventListener('submit',validateNewForm);
-login_form.addEventListener('submit',validateLoginForm);
-edit_form.addEventListener('submit',validateEditForm);
+// signup_form.addEventListener('submit',validateSignupForm);
+// new_form.addEventListener('submit',validateNewForm);
+// login_form.addEventListener('submit',validateLoginForm);
+// edit_form.addEventListener('submit',validateEditForm);
 
